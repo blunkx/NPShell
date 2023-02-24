09afc104e4260773ce5d19c25075785aec66278f
Kuan-Lin Chen <asd9521889@gmail.com>
2023-02-24 16:30:42 +0800

    add input parser that handle pipe and num pipe

--- source/parser.cc
+++ source/parser.cc
@@ -21,58 +21,113 @@ BUILT_IN_COM_E is_built_in_command(const vector<string> tokens)
     else
         return NOT_BUILT_IN;
 }
-// bool is_command(const string &token)
-// {
-// }
+TOKEN_TYPE_E which_type(const string &token)
+{
+    struct stat buffer;
+    if (stat(token.c_str(), &buffer) == 0)
+    {
+        return CMD;
+    }
+    else
+        return ARG;
+    // if (token == "setenv")
+    //     return SETENV;
+    // else if (token == "printenv")
+    //     return PRINTENV;
+    // else if (token == "exit")
+    //     return EXIT;
+    // else
+    //     return NOT_BUILT_IN;
+}
 
+// bool is_pipe_or_rd(const string &token)
+// {
+//     if (token == "|" || token == "!" || token == ">")
+//         return true;
+//     else
+//         return false;
+// }
+void split_by_pipe(vector<string> &tokens, vector<vector<string>> &cmds)
+{
+    vector<string>::iterator pre_end = tokens.begin();
+    pre_end = tokens.begin();
+    for (vector<string>::iterator it = tokens.begin(); it != tokens.end(); it++)
+    {
+        if (*it == "|" || *it == "!" || *it == ">")
+        {
+            if (pre_end != tokens.begin())
+                pre_end += 1;
+            vector<string> temp(pre_end, it);
+            cmds.push_back(temp);
+            pre_end = it;
+        }
+        else if (it == tokens.end() - 1)
+        {
+            if (*(pre_end) == "|" || *(pre_end) == "!" || *(pre_end) == ">")
+                pre_end += 1;
+            vector<string> temp(pre_end, tokens.end());
+            cmds.push_back(temp);
+        }
+    }
+}
+void split_by_number_pipe(vector<string> &tokens, vector<vector<string>> &num_pipes, vector<vector<string>> &cmds)
+{
+    vector<string>::iterator pre_end = tokens.begin();
+    vector<string> tokens_after_num_pipe;
+    for (vector<string>::iterator it = tokens.begin(); it != tokens.end(); it++)
+    {
+        if (std::regex_match((*it), std::regex("\\|\\d+")))
+        {
+            // cout << *it << endl;
+            // int n = stoi((*it).erase(0, 1));
+            // cout << n << endl;
+            if (pre_end != tokens.begin())
+                pre_end += 1;
+            vector<string> temp(pre_end, it);
+            num_pipes.push_back(temp);
+            pre_end = it;
+        }
+        else if (it == tokens.end() - 1)
+        {
+            if (std::regex_match((*pre_end), std::regex("\\|\\d+")))
+                pre_end += 1;
+            vector<string> temp(pre_end, tokens.end());
+            split_by_pipe(temp, cmds);
+        }
+    }
+    cout << "num_pipe" << endl;
+    for (int i = 0; i < num_pipes.size(); i++)
+    {
+        for (int j = 0; j < num_pipes[i].size(); j++)
+        {
+            cout << "<" << num_pipes[i][j] << "> ";
+        }
+        cout << endl;
+    }
+    cout << "cmds" << endl;
+    for (int i = 0; i < cmds.size(); i++)
+    {
+        for (int j = 0; j < cmds[i].size(); j++)
+        {
+            cout << "<" << cmds[i][j] << "> ";
+        }
+        cout << endl;
+    }
+}
 void parser(string &input)
 {
     if (input.empty())
         return;
-
     vector<string> tokens;
-    // cout << input << endl;
     istringstream iss(input);
-    copy(istream_iterator<string>(iss),
-         istream_iterator<string>(),
-         back_inserter(tokens));
-
+    copy(istream_iterator<string>(iss), istream_iterator<string>(), back_inserter(tokens));
+    vector<vector<string>> num_pipes;
+    vector<vector<string>> cmds;
+    split_by_number_pipe(tokens, num_pipes, cmds);
     switch (is_built_in_command(tokens))
     {
     case NOT_BUILT_IN:
-        // pid_t pid;
-        // int status;
-        exe_bin(tokens);
-
-        for (vector<string>::iterator it = tokens.begin(); it != tokens.end(); it++)
-        {
-            // if token is command
-            //      iter until next command or pipe
-            // else
-            //      discard and show in stderr
-            //      dash(ex: -n) => arg for previous command(including unkhown) => discard it
-            cout << *it << endl;
-
-            // pid = fork();
-            // if (pid == -1)
-            // {
-            // }
-            // else if (pid == 0)
-            // {
-            //     const char executable[] = "/bin/ls";
-            //     execl("/bin/ls", executable, "/", NULL);
-            // }
-        }
-        // for (vector<string>::iterator it = tokens.begin(); it != tokens.end(); it++)
-        //     waitpid(0, &status, 0);
-        // while (wait(&status) > 0)
-        // {
-        // }
-        /* why not
-        wait(NULL);
-        waitpid(0, &status, 0); // wait for the last child to exit
-        */
-        // waitpid(pid, &status, 0); // wait for the last child to exit
+        exe_bin(cmds);
         cout << "out_loop(parent):" << getpid() << endl;
         break;
     case SETENV:
