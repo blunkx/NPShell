1eb33ce9a05e01e04863b8406fe1562635af9fcb
Kuan-Lin Chen <asd9521889@gmail.com>
2023-03-02 17:21:49 +0800

    add type enum and command class

--- include/command.h
+++ include/command.h
@@ -3,6 +3,7 @@
 #define COMMAND_H_
 
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -10,11 +11,35 @@
 #include <cstdlib>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 using namespace std;
 
+typedef enum PIPE_TYPE_T
+{
+    NO_PIPE,
+    PIPE,
+    ERR_PIPE,
+    F_RED_PIPE,
+    NUM_PIPE,
+    ERR_NUM_PIPE,
+
+} PIPE_TYPE_E;
+
+class command
+{
+private:
+public:
+    PIPE_TYPE_E pipe_type;
+    int pipe_num = 0;
+    vector<string> cmd;
+    string which_type();
+};
+
 void init_env();
 void print_env(const char *const para);
-void exe_bin(vector<vector<string>> cmds);
+void exe_bin(vector<command> cmds);
 
 #endif
