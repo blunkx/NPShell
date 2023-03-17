33257775f2da23cf2c9c88eb2beb100e6e494d61
blunkx <asd9521889@gmail.com>
2023-03-18 00:57:50 +0800

    delete bug.sh

--- bug.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-cat demo/test_case/4.txt | ./npshell >demo/output/cat_4.txt 2>&1
-echo "cat 4.txt done"
-
-./npshell <demo/test_case/4.txt >demo/output/red_4.txt 2>&1
-echo "redirect 4.txt done"
