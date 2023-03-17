1984199fc89eb2dc64391288c867b948be06e751
blunkx <asd9521889@gmail.com>
2023-03-17 15:48:28 +0800

    change script to reproduce bug

--- bug.sh
+++ bug.sh
@@ -1,11 +1,7 @@
 #! /bin/sh
 
-cat demo/test_case/4.txt | ./npshell > demo/output/cat_4.txt 2>&1
+cat demo/test_case/4.txt | ./npshell >demo/output/cat_4.txt 2>&1
 echo "cat 4.txt done"
-cat demo/test_case/7.txt | ./npshell > demo/output/cat_7.txt 2>&1
-echo "cat 7.txt done"
 
-./npshell < demo/test_case/4.txt > demo/output/red_4.txt 2>&1
+./npshell <demo/test_case/4.txt >demo/output/red_4.txt 2>&1
 echo "redirect 4.txt done"
-./npshell < demo/test_case/7.txt > demo/output/red_7.txt 2>&1
-echo "redirect 7.txt done"
